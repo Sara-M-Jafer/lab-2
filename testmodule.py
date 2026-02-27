8221762221bd0937348da86d3c8df74940d21533
TUF <it25000143@avicenna.uobasrah.edu.iq>
2026-02-27 23:07:37 +0300

    sara correct the condition

--- testmodule.py
+++ testmodule.py
@@ -1,31 +1,60 @@
-class Person:
+from  abc import ABC,abstractmethod
+class Person(ABC):
     def __init__(self, name, age):
-        self.name = name
+        self._name = name
         self.age = age
 
+    @property
+    def age(self):
+        return self.__age
 
-def display_info(self):
-    print(f"Name: {self.name} | Age: {self.age}")
+    @age.setter
+    def age(self,new_age):
+        if 6 <= new_age <= 24 :
+            self.__age = new_age
+        else:
+            print("age must be between 6 and 24")
+
+    @abstractmethod
+    def display_info(self):
+        pass
 
 
 class Student(Person):
     def __init__(self, student_id, name, age, department, gpa):
         super().__init__(name, age)
         self.student_id = student_id
-        self.department = department
+        self.__department = department
         self.gpa = gpa
 
-    def display_info(self):
-        super().display_info()
-        print(f"Student_ID: {self.student_id} | Department: {self.department} | GPA:{self.gpa}")
+    @property
+    def id(self):
+        return self.student_id
 
-class Teacher(Person):
-    def __init__(self, teacher_id, name, age, department, salary):
-        super().__init__(name, age)
-        self.teacher_id = teacher_id
-        self.department = department
-        self.salary = salary
+    @property
+    def department(self):
+        return self.__department
+
+    @department.setter
+    def department(self,new_dpt):
+        self.__department = new_dpt
+
+    @property
+    def gpa(self):
+        return self.__gpa
+
+    @gpa.setter
+    def gpa(self,value):
+        if 0 <= value <= 4 :
+            self.__gpa = value
+        else:
+            print("gpa must be between 0 and 4")
+
+    def __str__(self):
+        return f"Student_ID: {self.student_id} | Name:{self._name} | Age:{self.age} | Department: {self.department} | GPA:{self.gpa}"
 
     def display_info(self):
-        super().display_info()
-        print(f"Teacher ID: {self.teacher_id} | Department: {self.department} | Salary: {self.salary}")
+        return f"Student_ID: {self.student_id} | Name:{self._name} | Age:{self.age} | Department: {self.department} | GPA:{self.gpa}"
+
+s =Student(1,"sara",30,"cybersecurity",3)
+print(s)
